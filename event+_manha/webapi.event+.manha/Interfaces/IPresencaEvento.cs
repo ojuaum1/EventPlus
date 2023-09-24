ac26ee146ba8a5b9f204502cd1ac320ab5cfeb10
João Oliveira <125273668+ojuaum1@users.noreply.github.com>
2023-09-24 17:38:02 -0300

    Update IPresencaEvento.cs

--- event+_manha/webapi.event+.manha/Interfaces/IPresencaEvento.cs
+++ event+_manha/webapi.event+.manha/Interfaces/IPresencaEvento.cs
@@ -1,13 +1,13 @@
 ﻿using apiweb.eventplus.manha.Domains;
-using webapi.event_.manha.Domains;
 
-namespace webapi.event_.manha.Interfaces
+namespace apiweb.eventplus.manha.Interfaces
 {
-    public interface IPresencaEvento
+    public interface IPresencaEventoRepository
     {
+        void Cadastrar(PresencaEvento presencaEvento);
         void Delete(Guid id);
         List<PresencaEvento> Listar();
-        PresencaEvento BuscarPorId(Guid id);
+        List<PresencaEvento> ListarMinhaPresenca(Guid id);
         void Atualizar(Guid id, PresencaEvento presencaEvento);
     }
 }
