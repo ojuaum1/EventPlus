7df4f2fb3b85ab77eef96578cf6b2cee58d85c83
João Oliveira <125273668+ojuaum1@users.noreply.github.com>
2023-09-24 17:35:37 -0300

    Update EventoRepository.cs

--- event+_manha/webapi.event+.manha/Repositories/EventoRepository.cs
+++ event+_manha/webapi.event+.manha/Repositories/EventoRepository.cs
@@ -1,28 +1,74 @@
-﻿using webapi.event_.manha.Domains;
-using webapi.event_.manha.Interfaces;
+﻿using apiweb.eventplus.manha.Contexts;
+using apiweb.eventplus.manha.Domains;
+using apiweb.eventplus.manha.Interfaces;
 
-namespace webapi.event_.manha.Repositories
+namespace apiweb.eventplus.manha.Repositories
 {
-    public class EventoRepository : IEvento
+    public class EventoRepository : IEventoRepository
     {
-        public void atualizar(Guid id, Evento evento)
+        private readonly EventContext _eventContext;
+
+        public EventoRepository()
         {
-            
+            _eventContext = new EventContext();
         }
 
-        public Evento BuscarPorId(Guid Id)
+        public void Atualizar(Guid id, Evento evento)
         {
-            throw new NotImplementedException();
+            Evento eventoAntigo = BuscarPorId(id);
+
+            if (eventoAntigo != null)
+            {
+                eventoAntigo.IdTipoEvento   = evento.IdTipoEvento;
+                eventoAntigo.IdInstituicao  = evento.IdInstituicao;
+                eventoAntigo.DataEvento     = evento.DataEvento;
+                eventoAntigo.Descricao      = evento.Descricao;
+                eventoAntigo.NomeEvento     = evento.NomeEvento;
+
+                _eventContext.Evento.Update(eventoAntigo);
+                _eventContext.SaveChanges();
+            }
+        }
+
+        public Evento BuscarPorId(Guid id)
+        {
+            Evento eventoBuscado = _eventContext.Evento.FirstOrDefault(z => z.IdEvento == id)!;
+
+            if (eventoBuscado != null)
+            {
+                return eventoBuscado;
+            }
+
+            return null!;
+
         }
 
         public void Cadastrar(Evento evento)
         {
-            throw new NotImplementedException();
+            if (evento != null)
+            {
+                _eventContext.Evento.Add(evento);
+                _eventContext.SaveChanges();
+            }
+
         }
 
-        public void Deletar(Guid id)
+        public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            Evento eventoDeletado = BuscarPorId(id);
+
+            if (eventoDeletado != null)
+            {
+                _eventContext.Evento.Remove(eventoDeletado);
+                _eventContext.SaveChanges();
+            }
+
+
+        }
+
+        public List<Evento> Listar()
+        {
+            return _eventContext.Evento.ToList();
         }
     }
 }
