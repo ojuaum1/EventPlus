5db7ef6f258a70e27c4b197425b2148598aff8d1
João Oliveira <125273668+ojuaum1@users.noreply.github.com>
2023-09-24 17:32:55 -0300

    Update InstituicaoController.cs

--- event+_manha/webapi.event+.manha/Controllers/InstituicaoController.cs
+++ event+_manha/webapi.event+.manha/Controllers/InstituicaoController.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using apiweb.eventplus.manha.Domains;
+using apiweb.eventplus.manha.Interfaces;
+using apiweb.eventplus.manha.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace webapi.event_.manha.Controllers
+namespace apiweb.eventplus.manha.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class InstituicaoController : ControllerBase
     {
+        IInstituicaoRepository _instituicaoRepository;
+        public InstituicaoController()
+        {
+            _instituicaoRepository = new InstituicaoRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Instituicao instituicao)
+        {
+            try
+            {
+                if (instituicao != null)
+                {
+                    _instituicaoRepository.Cadastrar(instituicao);
+                    return Ok("Usuario cadastrado!");
+
+                }
+
+                return Ok("Usuario não foi inserido corretamente!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
